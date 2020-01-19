19347eca05b6aa09ae5dfd58090c02e0dcdfce6e
kingdayx <54965488+kingdayx@users.noreply.github.com>
2020-01-18 23:14:15 -0500

    added layout and java
    added a button a listview and a text view gave them ids and the button an onclick method to update the textview field with the text searching and make it so that the button cant be pressed anymore.

--- app/src/main/java/com/daotec/bluetoothapp/MainActivity.java
+++ app/src/main/java/com/daotec/bluetoothapp/MainActivity.java
@@ -2,9 +2,24 @@ package com.daotec.bluetoothapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.bluetooth.BluetoothAdapter;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView listVIew = (ListView) findViewById(R.id.listView);
+    TextView statusTextView = (TextView) findViewById(R.id.statusTextView);
+    Button searchButton = (Button) findViewById(R.id.searchButton);
+    BluetoothAdapter bluetoothAdapter;
+
+    public void searchClicked(){
+        statusTextView.setText("Searching");
+        searchButton.setEnabled(false);
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
