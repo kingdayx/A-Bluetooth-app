2a7deca5649319f49a2a5056c84b5a47773b6584
kingdayx <54965488+kingdayx@users.noreply.github.com>
2020-01-21 04:16:50 -0500

    added permission for bluetooth and bluetooth functionality
    added uses-permission in android manifest for bluetooth adapter and added functionality for when button is cliced after searching for bluetooth devices is finished that text will be set to finished and reenable the button to search again.

--- app/src/main/java/com/daotec/bluetoothapp/MainActivity.java
+++ app/src/main/java/com/daotec/bluetoothapp/MainActivity.java
@@ -3,7 +3,13 @@ package com.daotec.bluetoothapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.bluetooth.BluetoothAdapter;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -14,12 +20,28 @@ public class MainActivity extends AppCompatActivity {
     TextView statusTextView;
     Button searchButton;
     BluetoothAdapter bluetoothAdapter;
+    IntentFilter filter;
 
-    public void searchClicked(){
+    private final BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            Log.i("action", action);
+
+            if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)){
+                statusTextView.setText("Finished");
+                searchButton.setEnabled(true);
+            }
+        }
+    };
+
+    public void searchClicked(View view){
         statusTextView.setText("Searching");
         searchButton.setEnabled(false);
+        bluetoothAdapter.startDiscovery();
 
     }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,5 +50,13 @@ public class MainActivity extends AppCompatActivity {
         listVIew = findViewById(R.id.listView);
         statusTextView =  findViewById(R.id.statusTextView);
         searchButton = findViewById(R.id.searchButton);
+
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        filter.addAction(BluetoothAdapter.ACTION_STATE_CHANGED);
+        filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_STARTED);
+        filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+        registerReceiver(broadcastReceiver, filter);
+
+
     }
 }
